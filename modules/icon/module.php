ae0003f68963c4445d13d49d1e44e2cdb879b8bd
Alexey Petlenko <alexeypetlenko@gmail.com>
2016-08-23 23:02:16 +0300

    - Padding group done

--- modules/icon/module.php
+++ modules/icon/module.php
@@ -421,6 +421,21 @@ class DSLC_Icon extends DSLC_Module {
 					//'margin_right' => 'post_var_id5',
 				),
 			),
+			array(
+				'label' => __( 'Padding', 'live-composer-page-builder' ),
+				'id' => 'first_grouppad',
+				'type' => 'group_padding',
+				'tab' => __( 'Some thing', 'live-composer-page-builder' ),
+				'section' => 'styling',
+				'affect_on_change_el' => '.dslc-micon-module',
+				'values' => array(
+					//'margin' => 'css_border_width',
+					//'margin_top' => 'css_border_radius',
+					//'margin_left' => 'post_var_id3',
+					'margin_bottom' => 'css_padding_bottom',
+					//'margin_right' => 'post_var_id5',
+				),
+			),
 		);
 
 		$dslc_options = array_merge( $dslc_options, $this->presets_options() );
