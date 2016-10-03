55817b0af29ad4c48827b52cabd28b0b13f4e15c
Alexey Petlenko <alexeypetlenko@gmail.com>
2016-10-03 20:36:31 +0300

    - First simple test

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -4,7 +4,7 @@ class SampleTest extends WP_UnitTestCase {
 
 	function testSample() {
 		// replace this with some actual testing code
-		$this->assertTrue( false );
+		$this->assertTrue( class_exists( 'DSLC_Module' ) );
 	}
 }
 
