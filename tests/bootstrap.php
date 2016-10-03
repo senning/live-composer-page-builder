f9b84ace6267ba34453c0bb60a4e2154cd75b2bf
Alexey Petlenko <alexeypetlenko@gmail.com>
2016-10-03 20:29:36 +0300

    - Added bin folder

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,8 @@ if ( !$_tests_dir ) $_tests_dir = __DIR__ . '/..';
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../sample-plugin.php';
+
+	require dirname( __FILE__ ) . '/../ds-live-composer.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
