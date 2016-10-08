2efaba4dd03679012991ad9708b495afcfbd18c2
Alexey Petlenko <alexeypetlenko@gmail.com>
2016-10-08 22:43:45 +0300

    - test 3

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -18,8 +18,8 @@ class SampleTest extends WP_UnitTestCase {
 
 		$current_user = new WP_User( 1 );
 		$current_user->set_role( 'administrator' );
-		wp_update_user( array( 'ID' => 1, 'first_name' => 'Admin', 'last_name' => 'User' ) );
-		//wp_set_current_user( 1 );
+
+		wp_set_current_user( 1 );
 		$new_id = dslc_get_new_module_id();
 		$this->assertInternalType( 'int', $new_id );
 		// replace this with some actual testing code
